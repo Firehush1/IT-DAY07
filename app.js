f5c399efd6e9c87ae1770e085590a6b6d49e19bb
Firehush1 <hansanikalhari99@gmail.com>
2026-08-10 14:43:20 +0530

    Add Swal.fire configuration to btnGuessNumberOnAction function

--- app.js
+++ app.js
@@ -5,5 +5,18 @@ let randomNum =Math.floor(num);
 console.log(randomNum);
 
 function btnGuessNumberOnAction() {
+    Swal.fire({
+  title: "Custom width, padding, color, background.",
+  width: 600,
+  padding: "3em",
+  color: "#716add",
+  background: "#fff url(/images/trees.png)",
+  backdrop: `
+    rgba(0,0,123,0.4)
+    url("/images/nyan-cat.gif")
+    left top
+    no-repeat
+  `
+});
     alert("haii");
 }
