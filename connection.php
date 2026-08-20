0578541d0cfa4261a729a0b98cd1c4fbc6298170
Firehush1 <hansanikalhari99@gmail.com>
2026-08-20 20:33:58 +0530

    Add Database class with static connection and setup method

---
+++ connection.php
@@ -0,0 +1,14 @@
+<?php
+   class Database
+   {
+    public static $connection;
+
+    public static function setUpconnection()
+    {
+        if (!isset()) {
+            # code...
+        }
+    }
+   }
+
+?>
